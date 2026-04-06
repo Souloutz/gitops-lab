6a46233b565755478d51884c7a0f3697f4c5c318
Souloutz <howardkong826@gmail.com>
2026-04-05 20:28:41 -0400

    fix syntax error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Git-Ops lab #4 with Argo CD! # missing quote
+    return "Hello from Git-Ops lab #4 with Argo CD (fixed)!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
